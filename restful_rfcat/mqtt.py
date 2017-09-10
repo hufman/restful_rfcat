afc54924064a5b37096d3a691f7d7e5c63915f4a
Walter Huf <hufman@gmail.com>
2017-09-09 22:28:01 -0700

    Refactors the MQTT Commanding class a bit

--- restful_rfcat/mqtt.py
+++ restful_rfcat/mqtt.py
@@ -23,29 +23,39 @@ class MQTTCommanding(object):
 		if rc != 0:
 			raise mqtt.MQTTException(mqtt.client.connack_string(rc))
 
-	def _on_message(self, mqttc, obj, msg):
+	def _find_device(self, path):
 		from restful_rfcat.web import device_list
+		parts = path.split('/', 1)
+		class_devices = device_list.get(parts[0], {})
+		device = class_devices.get(parts[1], None)
+		return device
+
+	def _set_device_state(self, path, state):
+		device = self._find_device(path)
+		if device is not None:
+			try:
+				logger.info("Setting state for %s to %s" % (path, state))
+				device.set_state(state)
+			except ValueError:
+				logger.warning("Invalid state for %s: %s" % (path, state))
+		else:
+			logger.warning("Could not find device at %s" % (path, ))
+
+	def _on_message(self, mqttc, obj, msg):
 		topic = msg.topic
 		data = msg.payload
+		logger.info("Incoming MQTT broadcast: %s %s" % (msg.topic, msg.payload))
 		if self.prefix is not None:
 			prefix = "%s/" % (self.prefix.rstrip('/'),)
 			topic = topic[len(prefix):]
-		parts = topic.split('/', 1)
-		class_devices = device_list.get(parts[0], {})
-		device = class_devices.get(parts[1], None)
-		if device is not None:
-			try:
-				logger.info("Setting state for %s to %s" % (topic, data))
-				device.set_state(data)
-			except ValueError:
-				logger.warning("Invalid state for %s: %s" % (topic, data))
+		self._set_device_state(topic, data)
 
 	def run(self):
 		self.client.connect(self.hostname, self.port, 60)
 		if self.prefix is not None:
-			self.client.subscribe("%s/#" % (self.prefix.rstrip('/'),))
+			self.client.subscribe("%s/+/+" % (self.prefix.rstrip('/'),))
 		else:
-			self.client.subscribe("#")
+			self.client.subscribe("+/+")
 		self.client.loop_forever()
 		
 	def stop(self):
