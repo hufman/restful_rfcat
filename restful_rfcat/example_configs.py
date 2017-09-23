56a9dd9fb97ad992bea4fd09d91607779de915ce
Walter Huf <hufman@gmail.com>
2017-09-23 13:43:57 -0700

    Adds unit tests for the example config generator

--- restful_rfcat/example_configs.py
+++ restful_rfcat/example_configs.py
@@ -9,6 +9,10 @@ except:
 
 from restful_rfcat import config
 
+# used for testing
+from restful_rfcat.drivers import FakeFan, FakeLight
+import mock
+
 def _inclusive_devices():
 	for d in config.DEVICES:
 		yield d
@@ -33,6 +37,17 @@ def get_curl(http_host):
 	return config
 
 def _openhab_item(d):
+	"""
+	>>> print(_openhab_item(FakeFan(name="fake", label="Fake")))
+	Dimmer rfcat_fans_fake "Fake" <fan_ceiling> [Switchable]
+	>>> print(_openhab_item(FakeLight(name="fake", label="Fake")))
+	Switch rfcat_lights_fake "Fake" <light> [Lighting]
+
+	>>> weird = FakeLight(name="fake", label="Fake")
+	>>> weird.CLASS = "custom"
+	>>> print(_openhab_item(weird))
+	Switch rfcat_custom_fake "Fake"
+	"""
 	if d.get_class() == 'fans':
 		return 'Dimmer rfcat_%s_%s "%s" <fan_ceiling> [Switchable]' % (d.get_class(), d.name, d.label)
 	if d.get_class() == 'lights':
@@ -40,6 +55,12 @@ def _openhab_item(d):
 	return 'Switch rfcat_%s_%s "%s"' % (d.get_class(), d.name, d.label)
 
 def _openhab_http_get(http_host, d):
+	"""
+	>>> print(_openhab_http_get("localhost:3350", FakeFan(name="fake", label="Fake")))
+	<[http://localhost:3350/fans/fake/command:300:REGEX((.*))]
+	>>> print(_openhab_http_get("localhost:3350", FakeLight(name="fake", label="Fake")))
+	<[http://localhost:3350/lights/fake:300:REGEX((.*))]
+	"""
 	if 'command' in d.subdevices:
 		d = d.subdevices['command']
 	return '<[http://%s/%s:300:REGEX((.*))]' % (
@@ -48,6 +69,12 @@ def _openhab_http_get(http_host, d):
 		)
 
 def _openhab_http_post(http_host, d):
+	"""
+	>>> print(_openhab_http_post("localhost:3350", FakeFan(name="fake", label="Fake")))
+	>[0:POST:http://localhost:3350/fans/fake/command:0] >[1:POST:http://localhost:3350/fans/fake/command:1] >[2:POST:http://localhost:3350/fans/fake/command:2] >[3:POST:http://localhost:3350/fans/fake/command:3]
+	>>> print(_openhab_http_post("localhost:3350", FakeLight(name="fake", label="Fake")))
+	>[OFF:POST:http://localhost:3350/lights/fake:OFF] >[ON:POST:http://localhost:3350/lights/fake:ON]
+	"""
 	if 'command' in d.subdevices:
 		d = d.subdevices['command']
 	return ' '.join((
@@ -60,6 +87,11 @@ def _openhab_http_post(http_host, d):
 	))
 
 def get_openhab_poll(http_host):
+	"""
+	>>> config.DEVICES = [FakeLight(name="fake", label="Fake")]
+	>>> print(get_openhab_poll('localhost:3350')['conf/items/rfcat.items'])
+	Switch rfcat_lights_fake "Fake" <light> [Lighting] { http="<[http://localhost:3350/lights/fake:300:REGEX((.*))] >[OFF:POST:http://localhost:3350/lights/fake:OFF] >[ON:POST:http://localhost:3350/lights/fake:ON]" }
+	"""
 	configs = {}
 	configs['conf/items/rfcat.items'] = '\n'.join((
 		'%s { http="%s %s" }' % (
@@ -71,11 +103,27 @@ def get_openhab_poll(http_host):
 	return configs
 
 def _openhab_mqtt_get(mqtt, d):
+	"""
+	>>> mqtt = mock.Mock()
+	>>> mqtt._set_topic = lambda x: x
+	>>> print(_openhab_mqtt_get(mqtt, FakeFan(name="fake", label="Fake")))
+	<[broker:fans/fake/command:state:REGEX((.*))]
+	>>> print(_openhab_mqtt_get(mqtt, FakeLight(name="fake", label="Fake")))
+	<[broker:lights/fake:state:REGEX((.*))]
+	"""
 	if 'command' in d.subdevices:
 		d = d.subdevices['command']
-	return '<[broker:%s:state:REGEX((.*))]' % (mqtt._set_topic(d._state_path),)
+	return '<[broker:%s:state:REGEX((.*))]' % (mqtt._set_topic(d._state_path()),)
 
 def _openhab_mqtt_post(mqtt_commanding, d):
+	"""
+	>>> mqtt = mock.Mock()
+	>>> mqtt.prefix = "command"
+	>>> print(_openhab_mqtt_post(mqtt, FakeFan(name="fake", label="Fake")))
+	>[broker:command/fans/fake/command:command:0:0] >[broker:command/fans/fake/command:command:1:1] >[broker:command/fans/fake/command:command:2:2] >[broker:command/fans/fake/command:command:3:3]
+	>>> print(_openhab_mqtt_post(mqtt, FakeLight(name="fake", label="Fake")))
+	>[broker:command/lights/fake:command:OFF:OFF] >[broker:command/lights/fake:command:ON:ON]
+	"""
 	def prefixed_path(path):
 		if mqtt_commanding.prefix:
 			return '%s/%s' % (mqtt_commanding.prefix.rstrip('/'), path)
@@ -92,6 +140,13 @@ def _openhab_mqtt_post(mqtt_commanding, d):
 	))
 
 def get_openhab_mqtt(http_host, mqtt):
+	"""
+	>>> mqtt = mock.Mock()
+	>>> mqtt._set_topic = lambda x: x
+	>>> config.DEVICES = [FakeLight(name="fake", label="Fake")]
+	>>> print(get_openhab_mqtt('localhost:3350', mqtt)['conf/items/rfcat.items'])
+	Switch rfcat_lights_fake "Fake" <light> [Lighting] { mqtt="<[broker:lights/fake:state:REGEX((.*))]" http=">[OFF:POST:http://localhost:3350/lights/fake:OFF] >[ON:POST:http://localhost:3350/lights/fake:ON]" }
+	"""
 	configs = {}
 	configs['conf/items/rfcat.items'] = '\n'.join((
 		'%s { mqtt="%s" http="%s" }' % (
@@ -103,6 +158,15 @@ def get_openhab_mqtt(http_host, mqtt):
 	return configs
 
 def get_openhab_mqtt_commanding(mqtt, mqtt_commanding):
+	"""
+	>>> mqtt = mock.Mock()
+	>>> mqtt._set_topic = lambda x: x
+	>>> mqtt_commanding = mock.Mock()
+	>>> mqtt_commanding.prefix = 'command'
+	>>> config.DEVICES = [FakeLight(name="fake", label="Fake")]
+	>>> print(get_openhab_mqtt_commanding(mqtt, mqtt_commanding)['conf/items/rfcat.items'])
+	Switch rfcat_lights_fake "Fake" <light> [Lighting] { mqtt="<[broker:lights/fake:state:REGEX((.*))] >[broker:command/lights/fake:command:OFF:OFF] >[broker:command/lights/fake:command:ON:ON]" }
+	"""
 	configs = {}
 	configs['conf/items/rfcat.items'] = '\n'.join((
 		'%s { mqtt="%s %s" }' % (
@@ -114,6 +178,30 @@ def get_openhab_mqtt_commanding(mqtt, mqtt_commanding):
 	return configs
 
 def get_hass(hass):
+	"""
+	>>> config.DEVICES = [FakeLight(name="fake", label="Fake"), FakeFan(name="fake", label="Fake")]
+	>>> from restful_rfcat.persistence import MQTTHomeAssistant
+	>>> print(get_hass(MQTTHomeAssistant(_publish=mock.Mock())))['.homeassistant/configuration.yml']  # doctest: +SKIP
+	fan:
+	 - platform: mqtt
+	   name: "Fake"
+	   state_topic: "homeassistant/fan/fans_fake/state"
+	   command_topic: "homeassistant/fan/fans_fake/set"
+	   speed_state_topic: "homeassistant/fan/fans_fake_speed/state"
+	   speed_command_topic: "homeassistant/fan/fans_fake_speed/set"
+	   payload_off: "OFF"
+	   payload_low_speed: "1"
+	   payload_medium_speed: "2"
+	   payload_high_speed: "3"
+	   speeds: ["low", "medium", "high"]
+	light:
+	 - platform: mqtt
+	   name: "Fake"
+	   state_topic: "homeassistant/light/lights_fake/state"
+	   command_topic: "homeassistant/light/lights_fake/set"
+	   payload_off: "OFF"
+	   payload_on: "ON"
+	"""
 	fans = [hass._device_config(d)
 	        for d in config.DEVICES
  	        if d.get_class() == 'fans']
